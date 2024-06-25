4c5864e3848209089752e41945e9eecc8b0fb5c0
YusungAN <52990109+YusungAN@users.noreply.github.com>
2024-06-25 15:37:46 +0900

    feat: responsive design util

---
+++ lib/util/responsive.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class Responsive {
+  BuildContext? context;
+  double deviceWidth = 0;
+  double deviceHeight = 0;
+
+  Responsive(context_) {
+    context = context_;
+    deviceWidth = MediaQuery.of(context_).size.width;
+    deviceHeight = MediaQuery.of(context_).size.height;
+  }
+
+  double percentWidth(double percent) {
+    return deviceWidth*(percent/100);
+  }
+
+  double percentHeight(double percent) {
+    return deviceHeight*(percent/100);
+  }
+}
