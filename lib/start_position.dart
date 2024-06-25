238d7df10d2d4170df0e3f607e9368b59c811928
YusungAN <52990109+YusungAN@users.noreply.github.com>
2024-06-25 15:36:14 +0900

    feat: 자세 측정 페이지 UI update
    responsive design

--- lib/start_position.dart
+++ lib/start_position.dart
@@ -1,83 +1,127 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class StartPosition  extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'util/responsive.dart';
+
+class StartPosition extends StatefulWidget {
+  const StartPosition({super.key});
+  @override
+  StartPositionState createState() => StartPositionState();
+}
+
+class StartPositionState extends State<StartPosition> {
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 390,
-          height: 844,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(color: Color(0xFFF9F9F9)),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 60,
-                top: 113,
-                child: Text(
-                  '바른 자세를 5초 동안 \n유지해주세요!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
-                  ),
+    Responsive responsive = Responsive(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+            child:Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 70),
+                  child: const Text(
+                    '바른 자세를 5초 동안 \n유지해주세요!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                    ),
+                  )
                 ),
-              ),
-              Positioned(
-                left: 60,
-                top: 295,
-                child: Container(
-                  width: 253,
-                  height: 253,
+                const SizedBox(height: 70),
+                Stack(
+                  children: [
+                    Container(
+                      width: responsive.percentWidth(70),
+                      height: responsive.percentWidth(70),
+                      decoration: const ShapeDecoration(
+                        color: Colors.white,
+                        shape: OvalBorder(),
+                        shadows: [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 0),
+                            spreadRadius: 1,
+                          )
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                        top: 9,
+                        left: 9,
+                        child: Container(
+                          width: responsive.percentWidth(70)-18,
+                          height: responsive.percentWidth(70)-18,
+                          decoration: const ShapeDecoration(
+                            color: Color(0xFF646464),
+                            shape: OvalBorder(),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              _lastTime,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xFFEBEBEB),
+                                fontSize: 80,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                          ),
+                        )
+                    )
+                  ],
+                ),
+                const SizedBox(height: 70),
+                Container(
+                  height: 35,
                   decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: OvalBorder(),
-                    shadows: [
+                    color: const Color(0xFF4A4A4A),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    shadows: const [
                       BoxShadow(
-                        color: Color(0x3F000000),
+                        color: Color(0x19000000),
                         blurRadius: 4,
-                        offset: Offset(0, 0),
-                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                        spreadRadius: 3,
                       )
                     ],
                   ),
-                ),
-              ),
-              Positioned(
-                left: 70,
-                top: 305,
-                child: Container(
-                  width: 234,
-                  height: 234,
-                  decoration: ShapeDecoration(
-                    color: Color(0xFF636363),
-                    shape: OvalBorder(),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 161,
-                top: 372,
-                child: Text(
-                  '5',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFFEBEBEB),
-                    fontSize: 80,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
-                  ),
-                ),
-              ),
-            ],
-          ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(responsive.percentWidth(40), 30),
+                      backgroundColor: const Color(0xFF4A4A4A),
+                      surfaceTintColor: const Color(0xFF4A4A4A),
+                      shadowColor: const Color(0x19000000)
+                    ),
+                    child: const Text(
+                      '기준 자세 측정',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                      ),
+                    )
+                  )
+                )
+              ],
+          )
         ),
-      ],
+      )
     );
   }
 }
