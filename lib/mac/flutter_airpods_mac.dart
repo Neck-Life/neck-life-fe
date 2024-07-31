ed80077cfa1ab705826dfae4b077070a97be7605
Moon-1C <dudtkd665@gmail.com>
2024-07-31 12:55:13 +0900

    fix!: mac용으로 작동하도록 수정

---
+++ lib/mac/flutter_airpods_mac.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_airpods/models/device_motion_data.dart';
+
+
+
+/// API for accessing information about the currently connected airpods.
+class FlutterAirpodsMac {
+  static const EventChannel _motionChannel =
+      EventChannel("flutter_airpods_mac.motion");
+
+  /// The getAirPodsDeviceMotionUpdates method allows to receive updates on the motion data of the currently connected airpods.
+  static Stream<DeviceMotionData> get getAirPodsDeviceMotionUpdates {
+    /// The data gets sent over the event channel.
+    /// Every incoming event gets read as a JSON and then
+    /// is mapped as [DeviceMotionData].
+    return _motionChannel.receiveBroadcastStream().map((event) {
+      Map<String, dynamic> json = jsonDecode(event);
+
+      /// Creates a [DeviceMotionData] from a JSON.
+      DeviceMotionData deviceMotionData = DeviceMotionData.fromJson(json);
+
+
+      /// Returns transformed [DeviceMotionData]
+      return deviceMotionData;
+    });
+  }
+}
